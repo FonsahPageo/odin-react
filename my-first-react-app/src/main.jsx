03ee15ae1e5332bd2eb1afdc07f80b84f2aaa69e
FonsahPageo <ashprincepageo@gmail.com>
2025-01-15 11:35:07 +0100

    intro to React testing: setting up react test environment

--- my-first-react-app/src/main.jsx
+++ my-first-react-app/src/main.jsx
@@ -2,10 +2,10 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 // import App from "./App.jsx";
-import { ClassInput } from "./practice.jsx";
+import { App } from "./practice.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ClassInput />
+    <App />
   </StrictMode>
 );
