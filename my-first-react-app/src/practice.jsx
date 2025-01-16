dd11080c6f8f15de5485a226a693c684abf969d4
FonsahPageo <ashprincepageo@gmail.com>
2025-01-16 10:59:28 +0100

    mocking callbacks and components

--- my-first-react-app/src/practice.jsx
+++ my-first-react-app/src/practice.jsx
@@ -1,20 +1,5 @@
-import { useState } from "react";
-
-const App = () => {
-  const [heading, setHeading] = useState("Magnificent Monkeys");
-
-  const clickHandler = () => {
-    setHeading("Radical Rhinos");
-  };
-
-  return (
-    <>
-      <button type="button" onClick={clickHandler}>
-        Click Me
-      </button>
-      <h1>{heading}</h1>
-    </>
-  );
+const CustomButton = ({ onClick }) => {
+  return <button onClick={onClick}>Click Me</button>;
 };
 
-export {App};
+export { CustomButton };
--- my-first-react-app/src/practice.test.jsx
+++ my-first-react-app/src/practice.test.jsx
@@ -1,22 +1,37 @@
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import { App } from "./practice.jsx";
+import { CustomButton } from "./practice.jsx";
 
-describe("App component", () => {
-  it("renders magnificent monkeys", () => {
-    const { container } = render(<App />);
-    expect(container).toMatchSnapshot();
+// "describe" is used to group related tests together
+// "it" is used to define individual test cases
+//  "expect" is used to create assertions on how the code should behave
+
+describe("CustomButton", () => {
+  it("should render a button with the text 'Click Me'", () => {
+    render(<CustomButton onClick={() => {}} />);
+
+    const button = screen.getByRole("button", { name: "Click Me" });
+
+    expect(button).toBeInTheDocument();
   });
 
-  it("render radical rhinos after button click", async () => {
+  it("should call the onClick function when clicked", async () => {
+    const onClick = vi.fn();
     const user = userEvent.setup();
+    render(<CustomButton onClick={onClick} />);
 
-    render(<App />);
     const button = screen.getByRole("button", { name: "Click Me" });
 
     await user.click(button);
 
-    expect(screen.getByRole("heading").textContent).toMatch(/radical rhinos/i);
+    expect(onClick).toHaveBeenCalled();
+  });
+
+  it("should not call the onClick function when it isn't clicked", async () => {
+    const onClick = vi.fn();
+    render(<CustomButton onClick={onClick} />);
+
+    expect(onClick).not.toHaveBeenCalled();
   });
 });
